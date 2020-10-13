c5b127c027011860751b35e62411909bbe3c9454
NeelChoksi <neelchoksi1611@gmail.com>
2020-10-13 16:06:48 +0530

    added error statements for signup

--- prisontry2/signup.php
+++ prisontry2/signup.php
@@ -1,9 +1,33 @@
 <?php
     require "header.php";
     if(isset($_POST['signup-submit-btn'])||isset($_GET['error'])){
-
+        if(isset($_GET['error'])){
+            if($_GET['error']=="emptyfields"){
+                echo'<style>h2{color:red;background:white;display:inline-block;} </style>
+                <div class=" container err"><h2 >Fill in all the fields !</h2></div>';
+    
+            }else  if($_GET['error']=="invaliduid"){
+                echo'<style>h2{color:red;background:white;display:inline-block;} </style>
+                <div class=" container err"><h2 >Enter Proper Username(use alpa and numbers) !</h2></div>';
+    
+            }else  if($_GET['error']=="passwordnotmatched"){
+                echo'<style>h2{color:red;background:white;display:inline-block;} </style>
+                <div class=" container err"><h2 >Rewrite the correct password !</h2></div>';
+    
+            }else  if($_GET['error']=="sameuserexistserror"){
+                echo'<style>h2{color:red;background:white;display:inline-block;} </style>
+                <div class=" container err"><h2 >A user already exists with this username!</h2></div>';
+    
+            }else  if($_GET['error']=="sqlerror"){
+                echo'<style>h2{color:red;background:white;display:inline-block;} </style>
+                <div class=" container err"><h2 >sqlerror!</h2></div>';
+    
+            }
+    
+        }
         echo'
     <main>
+    
     <div class="container">
             <h1>Add an admin</h1>
             <form autocomplete="off" action="includes/signup.inc.php" method="post">
@@ -17,8 +41,25 @@
     </div>
 
     </main>';
+
+  
+    }else if($_GET['signup']=="success"){
+        echo'
+        <div class="container"></div>
+        <main>
+        <style>h1{color:green; background:white;}a{font-weight:1000;text-align:center}</style>
+            <div class="container">
+                <form>
+                <h1>Signup Successful, now login by clicking the below link!</h1>
+                <a class="button" href="index.php">Click here to login</a>
+                </form>
+            </div>
+        </main>  
+        ';
+
     }else{
         echo'
+        <div class="container"></div>
         <main>
             <div class="container">
                 <form>
@@ -27,9 +68,6 @@
                 </form>
             </div>
         </main>  
-        
-        
-        
         ';
     }?>
 <?php
